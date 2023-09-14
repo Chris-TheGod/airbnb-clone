3e13f2472b82f2d16fd58484618b4f64f97fa6b4
Kristjan Lääne <xonproject@gmail.com>
2023-09-14 20:26:58 +0300

    some components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
+import './styles/styles.scss'
+
 function App() {
-  return <>Hi</>
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from '../images/airbnb-logo.png'
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <img src={logo} className='nav--logo' />
+    </nav>
+  )
+}
