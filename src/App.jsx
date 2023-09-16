2709a5987ca7e5e52934c1b0de4bf5d9f0344359
Kristjan Lääne <xonproject@gmail.com>
2023-09-16 13:44:20 +0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
 import './styles/styles.scss'
+import { Card } from './components/Card'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,20 @@
+import pic from '../images/katie-zaferes.png'
+import star from '../images/star.png'
+
 export const Card = () => {
-  return <div>Card</div>
+  return (
+    <div className='card'>
+      <img src={pic} className='card--image' />
+      <div className='card--stats'>
+        <img src={star} className='card--star' />
+        <span>5.0</span>
+        <span className='gray'>(6) • </span>
+        <span className='gray'>USA</span>
+      </div>
+      <p>Life Lessons with Katie Zaferes</p>
+      <p>
+        <span className='bold'>From $136</span> / person
+      </p>
+    </div>
+  )
 }
